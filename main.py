2fad4231d24e329ba90a633fd187438ef36ddaa6
mayevskaya <meowevskaya@gmail.com>
2023-05-18 22:26:09 +0500

    Sig parsing process fully implemented.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import time
-
 from playwright.async_api import async_playwright
-
-import logging
 
 from fastapi import FastAPI, UploadFile, File
 from fastapi.staticfiles import StaticFiles
@@ -11,16 +8,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # Configure CORS
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-logging.basicConfig(level=logging.INFO)
-
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
@@ -42,26 +36,20 @@ async def collect_upload(sig_file: UploadFile = File(...), pdf_file: UploadFile 
 
 async def parse_sig(pdf_file, sig_file):
     async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=False)
+        browser = await playwright.chromium.launch()
         page = await browser.new_page()
 
         await page.goto('https://www.gosuslugi.ru/eds')
 
         # Wait for the input elements to appear in the DOM
         await page.wait_for_selector('body')
-        logging.info("Gosuslugi page loaded successfully")
-        input_elements = await page.query_selector_all('input')
-
-        # Select the second and third input elements
-        input_element_2 = input_elements[1]
-        input_element_3 = input_elements[2]
+        input_element = await page.query_selector_all('input')
 
         # Fill in the input elements with the file contents
-        await input_element_2.set_input_files({'name': 'file.pdf', 'mimeType': 'application/pdf', 'buffer': pdf_file})
-        
-        # TO-DO: MAKE A NORMAL DELAY THERE
-        time.sleep(1)
-        await input_element_3.set_input_files({'name': 'file.sig', 'mimeType': 'application/octet-stream', 'buffer': sig_file})
+        await input_element[1].set_input_files({'name': 'file.pdf', 'mimeType': 'application/pdf', 'buffer': pdf_file})
+        await page.wait_for_selector('//span[@class="fileupload-custom-row-file-name ng-binding file-uploader-active"]')
+        time.sleep(2)
+        await input_element[2].set_input_files({'name': 'file.sig', 'mimeType': 'application/octet-stream', 'buffer': sig_file})
 
         await page.wait_for_selector("//a[@ng-bind='btnText']")
         button_el = await page.query_selector("//a[@ng-bind='btnText']")
@@ -70,7 +58,17 @@ async def parse_sig(pdf_file, sig_file):
         # Wait for conent we will extract to be loaded
         await page.wait_for_selector('//div[@class="row ng-scope"]')
 
-        output_html = await page.content()
+        # Select all the elements that contain information about signature
+        info = await page.query_selector_all('//*[@class="span_11 plain-text ng-binding"]')
+
+        parsed_dict = {
+            "validation": await info[0].text_content(),
+            "status": await info[1].text_content(),
+            "owner": await info[2].text_content(),
+            "publisher": await info[3].text_content(),
+            "id": await info[4].text_content(),
+            "valid_time": await info[5].text_content()
+        }
         await browser.close()
 
-        open("output.html", "w", encoding='utf-8').write(output_html)
+        open("t.txt", "w", encoding='utf-8').write(f"{parsed_dict}")
